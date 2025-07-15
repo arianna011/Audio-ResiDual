1b4da993c24934afd2587368f2d972660f88a2c2
Arianna Paolini <ariannapaolini011@gmail.com>
2025-07-15 10:32:14 +0200

    update init

--- data_processing/__init__.py
+++ data_processing/__init__.py
@@ -1,2 +1,2 @@
-from audio_dataset import AudioDataset
+from audio_dataset import AudioDataset, ESC_50_URL, ESC_50_OUT, ESC_50_AUDIO_DIR, ESC_50_META_FILE
 from download_utils import download_dataset, get_dataframe
